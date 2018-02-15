7017c95be437ee8dd53013a43d0460244d5e8b3c
Berry  Semexan <bsemexan@gmail.com>
2018-02-15 22:09:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,6 @@
 
 def key_for_min_value(name_hash)
   min_key = nil
-  name_hash.each { |key, value| min_key = value if value < min_key }
+  name_hash.each { |key, value| min_key = value if min_key.nil? || value < min_key }
   name_hash.key(min_key)
 end

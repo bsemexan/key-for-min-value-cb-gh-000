9d1d6faaa4759d408d9a1e1f1e1ad2cba5219248
Berry  Semexan <bsemexan@gmail.com>
2018-02-15 21:21:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |key, value|
+  name_hash.map do |key, value|
     key if value.size < value.size + 1
   end
 end
